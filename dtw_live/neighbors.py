f880ae79299b88c4a31de6eeb31981f5378f6434
Johann von Tiesenhausen <13jvt@queensu.ca>
2024-11-18 08:13:34 -0500

    lint

--- dtw_live/neighbors.py
+++ dtw_live/neighbors.py
@@ -11,14 +11,18 @@ from sklearn.utils.validation import check_array, check_X_y, check_is_fitted
 
 import dtw_live.viz as viz
 from dtw_live.base import ModelBase
-from dtw_live.dtw import (similarity_cost_matrix, warp_path,
-                          stream_similarity_cost_matrix)
+from dtw_live.dtw import (
+    similarity_cost_matrix,
+    warp_path,
+    stream_similarity_cost_matrix,
+)
 from dtw_live.dtw_c import update_cost_c
 from dtw_live.utils import to_padded_ndarray, transform_multioutput_ragged
 
 
-class OneVsRestStreamClassifier(BaseEstimator, ClassifierMixin,
-                                MultiOutputMixin, ModelBase):
+class OneVsRestStreamClassifier(
+    BaseEstimator, ClassifierMixin, MultiOutputMixin, ModelBase
+):
     """Custom Dynamic Time Warping Classifier for continuous time series
     data. Subsequence recognition is formulated as a series of separate binary
     classification problems, with classification based on the mode of
@@ -39,20 +43,19 @@ class OneVsRestStreamClassifier(BaseEstimator, ClassifierMixin,
         on the sample rate of your time series.
     metric : str, default='dtw'
         Distance metric to use.
-    metric_params : dict or None, default=None
-        Dictionary of metric parameters.
-        psi : float or int, default=0
-            Psi relaxation parameter for time series, represented as either a
-            relative size of series length (float between 0 and 1) or as an
-            absolute value (int).
-        groups : nested list with shape (n_groups,), default=None
-            Groups of feature indices for the multidimensional case used to
-            apply independent DTW [1]. If `None`, dependent DTW will be used.
-        window_size : float or int, default=1.0
-            Window size constraint of the Sakoe-Chiba band for DTW. Can be
-            represented as either a relative size of series length (float
-            between 0 and 1) or as an absolute value (int).
-            NOTE: Not implemented.
+    psi : float or int, default=0
+        Psi relaxation parameter for time series, represented as either a
+        relative size of series length (float between 0 and 1) or as an
+        absolute value (int).
+    use_mp : boolean, default=True
+        Use multiprocessing to compute DTW costs.
+    TODO: groups : nested list with shape (n_groups,), default=None
+        Groups of feature indices for the multidimensional case used to
+        apply independent DTW [1]. If `None`, dependent DTW will be used.
+    TODO: window_size : float or int, default=1.0
+        Window size constraint of the Sakoe-Chiba band for DTW. Can be
+        represented as either a relative size of series length (float
+        between 0 and 1) or as an absolute value (int).
     feature_names : list of str or None, default=None
         Feature names associated with input data features (optional). Required
         for online recognition.
@@ -67,15 +70,19 @@ class OneVsRestStreamClassifier(BaseEstimator, ClassifierMixin,
     knowledge discovery vol. 31,1 (2017): 1-31. doi:10.1007/s10618-016-0455-0
     """
 
-    def __init__(self,
-                 grace_time=20,
-                 metric='dtw',
-                 metric_params=None,
-                 feature_names=None,
-                 target_names=None):
+    def __init__(
+        self,
+        grace_time=20,
+        metric="dtw",
+        psi=0.0,
+        use_mp=True,
+        feature_names=None,
+        target_names=None,
+    ):
         self.grace_time = grace_time
         self.metric = metric
-        self.metric_params = metric_params
+        self.psi = psi
+        self.use_mp = use_mp
         self.feature_names = feature_names
         self.target_names = target_names
 
@@ -93,14 +100,11 @@ class OneVsRestStreamClassifier(BaseEstimator, ClassifierMixin,
 
         Returns
         -------
-        KNeighborsStreamClassifier object
+        OneVsRestStreamClassifier object
             The fitted classifier.
         """
         X = check_array(X, allow_nd=True, force_all_finite=False)
-        X, y = check_X_y(X, y,
-                         allow_nd=True,
-                         multi_output=True,
-                         force_all_finite=False)
+        X, y = check_X_y(X, y, allow_nd=True, multi_output=True, force_all_finite=False)
 
         # transform data/target streams to ragged query sequences
         if y.ndim == 2:
@@ -109,21 +113,19 @@ class OneVsRestStreamClassifier(BaseEstimator, ClassifierMixin,
             X = [q for q, t in zip(X, y) if t != -1]
             y = [t for t in y if t != -1]
 
+        # print([f"{str(target_names[t])}: {a.shape[0]}" for a, t in zip(X, y)])
+
         self.classes_, self.n_classes = np.unique(y, return_counts=True)
         if any(c < 2 for c in self.n_classes):
-            raise ValueError('At least 2 samples required per class')
+            raise ValueError("At least 2 samples required per class")
 
-        if self.metric_params is None:
-            self.metric_params = {}
-
-        if self.metric == 'dtw':
-            mats, ix = similarity_cost_matrix(X, **self.metric_params)
-            psi = self.metric_params.get('psi', 0.0)
-            if isinstance(psi, tuple):
-                raise ValueError('unbalanced psi values not allowed for fit')
-            elif (psi > 0.0):
+        if self.metric == "dtw":
+            mats, ix = similarity_cost_matrix(X, psi=self.psi, use_mp=self.use_mp)
+            if isinstance(self.psi, tuple):
+                raise ValueError("unbalanced psi values not allowed for fit")
+            elif self.psi > 0.0:
                 # trim sequences based on warping path start/end points
-                paths = [warp_path(m, psi=psi) for m in mats]
+                paths = [warp_path(m, psi=self.psi) for m in mats]
                 dists = [m[p[-1]] for m, p in zip(mats, paths)]
 
                 tr = [(0, np.inf) for _ in paths]
@@ -132,13 +134,15 @@ class OneVsRestStreamClassifier(BaseEstimator, ClassifierMixin,
                     tr[j] = (max(tr[j][0], p[0][1]), min(tr[j][1], p[-1][1]))
 
                 # apply trimming iff trim is smaller than (1-psi)% of original
-                X = [x[ts:te+1, :] if (te - ts) > (1 - psi) * x.shape[0] else x
-                     for x, (ts, te) in zip(X, tr)]
+                X = [
+                    x[ts : te + 1, :] if (te - ts) > (1 - self.psi) * x.shape[0] else x
+                    for x, (ts, te) in zip(X, tr)
+                ]
                 X = to_padded_ndarray(X)
             else:
                 dists = [m[-1][-1] for m in mats]
         else:
-            raise NotImplementedError('%s' % self.metric)
+            raise NotImplementedError("%s" % self.metric)
 
         # sort distances index by target
         target_dists = [{k: [] for k in self.classes_} for _ in y]
@@ -152,7 +156,7 @@ class OneVsRestStreamClassifier(BaseEstimator, ClassifierMixin,
         for i, d in enumerate(target_dists):
             di_max = max(d[y[i]])
             dj_min = min(min(v) for k, v in d.items() if k != y[i])
-            if (di_max > dj_min):
+            if di_max > dj_min:
                 thr = di_max
             else:
                 thr = 0.5 * (dj_min - di_max) + di_max
@@ -161,8 +165,9 @@ class OneVsRestStreamClassifier(BaseEstimator, ClassifierMixin,
 
         self.debug_dists = None
         if self.target_names is not None:
-            self.debug_dists = [{self.target_names[k]: v for k, v in d.items()}
-                                for d in target_dists]
+            self.debug_dists = [
+                {self.target_names[k]: v for k, v in d.items()} for d in target_dists
+            ]
 
         # model fit params
         self.X_fit = X
@@ -174,7 +179,7 @@ class OneVsRestStreamClassifier(BaseEstimator, ClassifierMixin,
         return self
 
     def predict(self, X):
-        """Predict the class labels for the provided stream data. 
+        """Predict the class labels for the provided stream data.
 
         NOTE: This method resets the current prediction state used by
         :meth:`predict_frame`.
@@ -198,19 +203,20 @@ class OneVsRestStreamClassifier(BaseEstimator, ClassifierMixin,
 
         y_pred = []
         self._temp_mats = []
-        
+
         for data in X:
             self.reset()
 
-            mats, _ = stream_similarity_cost_matrix(data, self.X_fit,
-                                                    **self.metric_params)
+            mats, _ = stream_similarity_cost_matrix(
+                data, self.X_fit, psi=self.psi, use_mp=self.use_mp
+            )
             costs = [m[:, -1] for m in mats]
             cost_frames = np.transpose(costs)
             for c in cost_frames:
                 self._predict_frame_costs(c)
 
             y_pred += self.f_events
-            
+
             self._temp_mats.append(mats)
 
         return y_pred
@@ -243,13 +249,13 @@ class OneVsRestStreamClassifier(BaseEstimator, ClassifierMixin,
         """
         check_is_fitted(self)
         if sample_weight is not None:
-            raise NotImplementedError('sample_weight is not implemented')
+            raise NotImplementedError("sample_weight is not implemented")
 
         if X.ndim == 2:  # single sample
             X = X[np.newaxis, :]
 
-        # nt = len(self.target_names)
-        # self.conf_mat = np.zeros((nt + 1, nt + 1), dtype=int)
+        nt = len(self.target_names)
+        self.conf_mat = np.zeros((nt + 1, nt + 1), dtype=int)
 
         TP, FP, FN, TPR, FPR = (0, 0, 0, 0, 0)
         for data, targets in zip(X, y):
@@ -306,13 +312,13 @@ class OneVsRestStreamClassifier(BaseEstimator, ClassifierMixin,
             """
 
             # compare predicted events to ground truth
-            for l, ts, te in ground:
+            for true_label, ts, te in ground:
                 flag = False
 
-                for i, (e, t0, t1) in enumerate(events):
+                for i, (predict_label, t0, t1) in enumerate(events):
                     # event within bounds (P)
                     if ts < t0 <= te or ts < t1 <= te:
-                        if l == e:  # correct class (T)
+                        if true_label == predict_label:  # correct class (T)
                             if flag:
                                 TPR += 1  # repeated
                             else:
@@ -325,25 +331,33 @@ class OneVsRestStreamClassifier(BaseEstimator, ClassifierMixin,
 
                         flag = True
                         events.pop(i)
-                        # self.conf_mat[l + 1][e + 1] += 1
+                        self.conf_mat[true_label + 1][predict_label + 1] += 1
 
                 if not flag:  # no event found within bounds (FN)
                     FN += 1
-                    # self.conf_mat[l + 1][0] += 1
+                    self.conf_mat[true_label + 1][0] += 1
 
             # add any events not found within bounds (FP)
             FP += len(events)
 
-            # for (e, _, _) in events:  # add remaining events to null class
-            #     self.conf_mat[0][e + 1] += 1
+            for (
+                predict_label,
+                _,
+                _,
+            ) in events:  # add remaining events to null class (FP)
+                self.conf_mat[0][predict_label + 1] += 1
 
+        # Save results
+        self.score_results = {
+            "TP": TP,
+            "FP": FP,
+            "FN": FN,
+            "TP_Repeated": TPR,
+            "FP_Repeated": FPR,
+        }
         precision = TP / (TP + FP + FPR + TPR)
         recall = TP / (TP + FN)
         f1 = 2 * precision * recall / (precision + recall)
-
-        # debug print
-        print({'TP': TP, 'FP': FP, 'FN': FN, 'TPR': TPR, 'FPR': FPR, 'f1': f1})
-        print(f'precision: {100 * precision:.3f}, recall: {100 * recall:.3f}')
         return f1
 
     def reset(self):
@@ -375,7 +389,7 @@ class OneVsRestStreamClassifier(BaseEstimator, ClassifierMixin,
         array-like with shape (n_timesteps,) or (n_samples, n_timesteps)
             Array of predicted class labels and start/end indices.
         """
-        if not (X.flags['C_CONTIGUOUS'] or X.dtype == np.float64):
+        if not (X.flags["C_CONTIGUOUS"] or X.dtype == np.float64):
             X = np.ascontiguousarray(X, dtype=np.float64)
 
         costs = []
@@ -405,7 +419,7 @@ class OneVsRestStreamClassifier(BaseEstimator, ClassifierMixin,
             Predicted events for each template
         """
         if len(costs) != np.sum(self.n_classes):
-            raise ValueError('Query costs/count mismatch.')
+            raise ValueError("Query costs/count mismatch.")
 
         for i, cost in enumerate(costs):
             # update event buffer
@@ -421,7 +435,7 @@ class OneVsRestStreamClassifier(BaseEstimator, ClassifierMixin,
         # check event durations
         e_durs = []
         for start, _, _ in self.e_buf.values():
-            flag = ((self.f_cnt - start) >= self.grace_time)
+            flag = (self.f_cnt - start) >= self.grace_time
             e_durs.append(flag)
 
         # process events if durations elapsed
@@ -431,11 +445,10 @@ class OneVsRestStreamClassifier(BaseEstimator, ClassifierMixin,
             # i, e = min(self.e_buf.items(), key=lambda x: x[1][2])
 
             # get event index + average buffer
-            # NOTE: multiple choices are currently selected at random
+            # NOTE: first event is picked in the event of multiple choices
             counts = Counter(et).most_common(len(self.classes_))
-            modes = [l for l, c in counts if c == counts[0][1]]
-            e = random.choice(modes)
-            # e = modes[0]
+            modes = [label for label, c in counts if c == counts[0][1]]
+            e = modes[0]
             eb = [v for k, v in self.e_buf.items() if self.y_fit[k] == e]
             i0, i1, _ = np.mean(eb, axis=0)
 
@@ -463,15 +476,20 @@ class OneVsRestStreamClassifier(BaseEstimator, ClassifierMixin,
         return y_pred
 
     def _to_dict(self):
-        """Return a dict representation of the model (for serialization)
-        """
+        """Return a dict representation of the model (for serialization)"""
         params = self.get_params()
-        fit_params = ['classes_', 'n_classes', 'thresholds_fit',
-                      'X_fit', 'y_fit', 'debug_dists']
+        fit_params = [
+            "classes_",
+            "n_classes",
+            "thresholds_fit",
+            "X_fit",
+            "y_fit",
+            "debug_dists",
+        ]
 
         for k in fit_params:
             if k in params:
-                raise KeyError('key %s already exists in model params' % k)
+                raise KeyError("key %s already exists in model params" % k)
 
             params[k] = getattr(self, k)
 
